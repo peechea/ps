a36cbe498cc589c8873f6647037fed800a6db919
พิทยา มุขดี <peecheamookdee@gmail.com>
2019-06-25 16:40:25 +0700

    test

---
+++ PS/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+        public ActionResult Seat()
+        {
+            return View();
+        }
+
+        public ActionResult Reserve()
+        {
+            return View();
+        }
+
+        public ActionResult Reserve2()
+        {
+            return View();
+        }
+    }
+}
---
+++ PS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PS.MvcApplication" Language="C#" %>
